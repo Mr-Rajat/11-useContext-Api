b129d4b1cea1257564b7b1c9cd99bb612aeded89
Rajat Choudhary <rajatch133@gmail.com>
2023-07-16 19:53:05 +0530

    Used Context Api in whole comp & left to index.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, {useContext} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
-import Authcontext from './store/auth-context';
+import AuthContext from "./store/auth-context";
 
 function App() {
-
-  // console.log(typeof(storedUserLoggedInInformation));
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-    localStorage.removeItem("isLoggedIn");
-
-  };
-
-  useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
-
-    if (storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true);
-    }
-
-
-  }, [])
+  const context = useContext(AuthContext);
+  const {onLogin, onLogout, isLoggedIn} = context;
 
   return (
     // {/* wrapped with context and provided */}
-    // <React.Fragment>
-      <Authcontext.Provider value={{
-        isLoggedIn: isLoggedIn,
-        onLogout: logoutHandler
-      }}>
-        <MainHeader />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
-      </Authcontext.Provider>
-    // </React.Fragment>
+    <React.Fragment>
+
+      <MainHeader />
+      <main>
+        {!isLoggedIn && <Login />}
+        {isLoggedIn && <Home />}
+      </main>
+
+    </React.Fragment>
   );
 }
 
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Authcontext = React.createContext({
-    isLoggedIn: false
+const AuthContext = React.createContext({
+    isLoggedIn: false,
+    onLogout: () => { },
+    onLogin: (email, password) => { }
 });
+// better practice to pass all props or values inside main context Api for suggestion of available props where we use it.
 
-export default Authcontext;
+// if we want to pass multiple props then its better to create different Provider for better management.
+
+export const AuthContextProvider = (props) => {
+    // console.log(typeof(storedUserLoggedInInformation));
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+
+
+    }, [])
+
+    const logoutHandler = () => {
+        localStorage.removeItem("isLoggedIn");
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+
+
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{
+                isLoggedIn: isLoggedIn,
+                onLogin: loginHandler,
+                onLogout: logoutHandler
+            }}>
+            {props.children}
+        </AuthContext.Provider>)
+}
+export default AuthContext;
